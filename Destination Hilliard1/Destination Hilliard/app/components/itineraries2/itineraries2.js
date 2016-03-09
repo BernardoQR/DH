dce48cdfd602b1dadbf4e900b7e3de3660e2029b
Bernardo Rangel <brangel@rippleeffectweb.com>
2016-03-09 16:23:12 -0500

    Testing itineraries v2.0

---
+++ Destination Hilliard1/Destination Hilliard/app/components/itineraries2/itineraries2.js
@@ -0,0 +1,126 @@
+'use strict';
+var isInit = true,
+    helpers = require('../../utils/widgets/helper'),
+    service = require('./itineraries2-service'),
+    // additional requires
+    viewModel = require('./itineraries2-view-model');
+var observableArrayModule = require("data/observable-array");
+
+
+function pageLoaded(args) {
+    var page = args.object;
+
+    helpers.platformInit(page);
+    page.bindingContext = viewModel;
+
+    viewModel.set('isLoading', true);
+    var itemsList = [];
+    service.getAllRecords()
+        .then(function (result) {
+
+            result.forEach(function (item) {
+                flattenLocationProperties(item);
+                itemsList.push({
+                    itineraryTitle: item.Title,
+                    itineraryDescription: item.Description,
+                    itineraryId: item.Id,
+                    itineraryItems: [],
+                    visibility: "collapsed"
+                });
+            });
+            viewModel.set('listItems', itemsList);
+            viewModel.set('isLoading', false);
+        })
+        .catch(function onCatch() {
+            //viewModel.set('isLoading', false);
+            alert("error2")
+        });
+
+
+
+    if (isInit) {
+        isInit = false;
+        // additional pageInit
+    }
+}
+
+
+
+function onListViewItemTap(args) {
+    viewModel.set('isLoading', true);
+    var itemsList = [];
+    var temp = [];
+    itemsList = GetListItems();
+    var itemData = viewModel.get('listItems')[args.index];
+
+    var ChildItems = [];
+    service.getDynamicContent(itemData.itineraryId)
+        			.then(function (SubItems) {
+        			    ChildItems = [];
+        			    SubItems.forEach(function (dynamicContent) {
+        			        flattenLocationProperties(dynamicContent);
+        			        ChildItems.push({
+        			            pointOfInterestId: dynamicContent.Id,
+        			            pointOfInterestTitle: dynamicContent.Title,
+        			            pointOfInterestDescription: dynamicContent.Description
+        			        });
+        			    });
+        			    itemsList[args.index].visibility = "visible";
+        			    itemsList[args.index].itineraryItems = ChildItems;
+        			    temp.push(itemsList[args.index]);
+        			    viewModel.set('listItems', "");
+        			    viewModel.set('listItems', itemsList);
+        			})
+         .catch(function onCatch() {
+             //viewModel.set('isLoading', false);
+             alert("error2")
+         });
+}
+
+
+function onDetailItemTap(args) {
+    alert("AAAAAAAAAA")
+}
+
+function GetListItems() {
+    var itemsList = [];
+    itemsList = viewModel.get('listItems');
+    itemsList.forEach(function (item) {
+        item.visibility = "collapsed";
+        item.itineraryItems = "";
+    });
+    return itemsList;
+}
+
+
+
+
+function flattenLocationProperties(dataItem) {
+    var propName, propValue,
+        isLocation = function (value) {
+            return propValue && typeof propValue === 'object' &&
+                propValue.longitude && propValue.latitude;
+        };
+
+    for (propName in dataItem) {
+        if (dataItem.hasOwnProperty(propName)) {
+            propValue = dataItem[propName];
+            if (isLocation(propValue)) {
+                dataItem[propName] =
+                    'Latitude: ' + propValue.latitude +
+                    'Longitude: ' + propValue.longitude;
+            }
+        }
+    }
+}
+// additional functions
+
+
+
+// START_CUSTOM_CODE_itineraries2
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_itineraries2
+exports.pageLoaded = pageLoaded;
+exports.onListViewItemTap = onListViewItemTap;
+exports.onDetailItemTap = onDetailItemTap;
