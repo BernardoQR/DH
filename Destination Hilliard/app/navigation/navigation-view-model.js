663a9820a1ddb1d7f416cdf1bf4a7b6f1b20834a
Daniel Klinzing <dklinzing@rippleeffectweb.com>
2016-03-11 14:02:26 +0000

    First commit

--- Destination Hilliard/app/navigation/navigation-view-model.js
+++ Destination Hilliard/app/navigation/navigation-view-model.js
@@ -7,6 +7,34 @@ menuItems = [{
     "title": "Home View",
     "moduleName": "components/homeView/homeView",
     "icon": "\ue0dd"
+}, {
+    "title": "Places To Eat",
+    "moduleName": "components/placesToEat/placesToEat",
+    "icon": "\ue0eb"
+}, {
+    "title": "Places To Stay",
+    "moduleName": "components/placesToStay/placesToStay",
+    "icon": "\ue0eb"
+}, {
+    "title": "Things To Do",
+    "moduleName": "components/thingsToDo/thingsToDo",
+    "icon": "\ue0eb"
+}, {
+    "title": "Itineraries",
+    "moduleName": "components/itineraries/itineraries",
+    "icon": "\ue0eb"
+}, {
+    "title": "Events",
+    "moduleName": "components/events/events",
+    "icon": "\ue0eb"
+}, {
+    "title": "Contact Us",
+    "moduleName": "components/contactUs/contactUs",
+    "icon": "\ue0eb"
+}, {
+    "title": "Map",
+    "moduleName": "components/map/map",
+    "icon": "\ue0eb"
 }];
 
 navigationViewModel.set('menuItems', menuItems);
